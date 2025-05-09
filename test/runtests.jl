f02035d66800eac48cba0183f10cd16e705e0776
stensmo <141365386+stensmo@users.noreply.github.com>
2025-05-09 11:06:34 +0200

    Cleaned tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,5 @@
-using SFTPClient
+using WiringPi
 using Test
 
 
 include("setup.jl")
-
-@testset "Connect Test" begin
-
-    @test files == actualFiles
-    @test stats[1] == actualStructs[1]
-    @test isfile(tempDir * "KeyGenerator.png")
-    @test dirs == ["example"]
-    @test isfile("readme.txt")
-    @test walkdirFiles[3] == walkdirResults[3]
-
-end
-
